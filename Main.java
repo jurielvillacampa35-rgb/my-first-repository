0b83d135689dd2096ca0fc9ef5815ded58be68cf
Juriel Villacampa <jurielvillacampa35@gmail.com>
2026-09-17 17:54:05 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+public class Main {
+ public static void main(String[] args) {
+ 
+ Movie m1 = new Movie();
+ m1.title = "The Hangover";
+ m1.genre = "Comedy";
+ m1.duration = 100;
+ 
+ m1.displayInfo();
+ 
+ Movie m2 = new Movie();
+ m2.title = "Avengers: Endgame";
+ m2.genre = "Action";
+ m2.duration = 181;
+ 
+ m2.displayInfo();
+ 
+ Movie m3 = new Movie();
+ m3.title = "The Notebook";
+ m3.genre = "Romance";
+ m3.duration = 123;
+ 
+ m3.displayInfo();
+
+ 
+
+ 
+ }
+}
---
+++ Movie.java
@@ -0,0 +1,12 @@
+public class Movie {
+ 
+ String title;
+ String genre;
+ int duration;
+   
+   void displayInfo() {
+    System.out.println(title + " (" + genre + ") - " + duration + " minutes");
+    
+    }
+    
+ }
