53b05ca9b36a5fc03cb85d884460c94db6365e34
Juriel Villacampa <jurielvillacampa35@gmail.com>
2026-09-21 12:48:53 +0800

    Replace Vehicle instances with Movie instances

--- Main.java
+++ Main.java
@@ -1,40 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-       
-        Vehicle v1 = new Vehicle();
-        v1.brand = "Ford";
-        v1.model = "Mustang";
-        v1.year = 1967;
+        Movie m1 = new Movie();
+        m1.title = "Avengers: Endgame";
+        m1.genre = "Action";
+        m1.duration = 181;
         
+        m1.displayInfo();
+
+        Movie m2 = new Movie();
+        m2.title = "The Hangover";
+        m2.genre = "Comedy";
+        m2.duration = 100;
         
-        System.out.println("--- VEHICLE 1 ---");
-        v1.displayInfo();
-        System.out.println("Age: " + v1.calculateAge());
-        System.out.println("Vintage? " + v1.isVintage());
+        m2.displayInfo();
 
-
-        Vehicle v2 = new Vehicle();
-        v2.brand = "Toyota";
-        v2.model = "Corolla";
-        v2.year = 2018;
+        Movie m3 = new Movie();
+        m3.title = "The Notebook";
+        m3.genre = "Romance";
+        m3.duration = 123;
         
-        System.out.println("--- VEHICLE 2 ---");
-        v2.displayInfo();
-        System.out.println("Age: " + v2.calculateAge());
-        System.out.println("Vintage? " + v2.isVintage());
-
-
-        Vehicle v3 = new Vehicle();
-        v3.brand = "Honda";
-        v3.model = "Civic";
-        v3.year = 2023;
-        
-        System.out.println("--- VEHICLE 3 ---");
-        v3.displayInfo();
-        System.out.println("Age: " + v3.calculateAge());
-        System.out.println("Vintage? " + v3.isVintage());
-
-
-            }
-}  
- 
+        m3.displayInfo();
+    }
+}
